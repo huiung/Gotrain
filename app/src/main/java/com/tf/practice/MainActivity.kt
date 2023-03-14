283bc77a973e02a141ae4b126ed8a7c2ec281113
huiung <gmldnd0418@gamil.com>
2023-03-14 23:48:57 +0900

    MainActivity 구현 >
    permission 및 사진 촬영, 갤러리 얻어오는 동작은 Activity Result API 활용.

--- app/src/main/java/com/tf/practice/MainActivity.kt
+++ app/src/main/java/com/tf/practice/MainActivity.kt
@@ -1,11 +1,86 @@
 package com.tf.practice
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.FileProvider
+import androidx.lifecycle.lifecycleScope
+import com.tf.practice.base.BaseActivity
+import com.tf.practice.databinding.ActivityMainBinding
+import java.io.File
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
+
+    companion object {
+        const val ARG_URI = "ARG_URI"
+    }
+
+    private val permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+        if (isGranted) {
+            takeImage()
+        }
+    }
+
+    private val cameraLauncher = registerForActivityResult(ActivityResultContracts.TakePicture()) { isSuccess ->
+
+        if (isSuccess) {
+            latestTmpUri?.let { uri ->
+                val intent = Intent(baseContext, InspectActivity::class.java)
+                intent.putExtra(ARG_URI, uri)
+                startActivity(intent)
+            }
+        }
+    }
+
+    private val galleryLauncher = registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+
+        uri ?: return@registerForActivityResult
+
+        val intent = Intent(baseContext, InspectActivity::class.java)
+        intent.putExtra(ARG_URI, uri)
+        startActivity(intent)
+    }
+
+    private var latestTmpUri: Uri? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding.infoTextView.setOnClickListener {
+            val intent = Intent(baseContext, InfoActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.cameraButton.setOnClickListener {
+            if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                takeImage()
+            } else {
+                permissionLauncher.launch(Manifest.permission.CAMERA)
+            }
+        }
+        binding.galleryButton.setOnClickListener {
+            galleryLauncher.launch("image/*")
+        }
+    }
+
+    private fun takeImage() {
+        lifecycleScope.launchWhenStarted {
+            getTmpFileUri().let { uri ->
+                latestTmpUri = uri
+                cameraLauncher.launch(uri)
+            }
+        }
+    }
+
+    private fun getTmpFileUri(): Uri {
+        val tmpFile = File.createTempFile("tmp_image_file", ".png", cacheDir).apply {
+            createNewFile()
+            deleteOnExit()
+        }
+
+        return FileProvider.getUriForFile(applicationContext, "${BuildConfig.APPLICATION_ID}.provider", tmpFile)
     }
 }
